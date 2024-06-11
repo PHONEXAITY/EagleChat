a867541a66594fba674dd61a4d3eb8e075c84aba
PHONEXAI <148427675+PHONEXAITY@users.noreply.github.com>
2024-06-11 11:08:03 +0700

    Update-Cors

--- src/server.js
+++ src/server.js
@@ -21,15 +21,6 @@ connectDB();
 //Routes
 app.use('/user',userRouter);
 app.use('/user/messages', messageRouter);
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(  "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization" );
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-      return res.status(200).json({});
-    }
-    next();
-  });
 const PORT = process.env.PORT;
 
 server.listen(PORT, ()=>{
